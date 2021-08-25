422dfe020c828fdbc6aec9e066442d13a6d32fbe
Suparth Jain <suparthjain22@gmail.com>
2021-08-25 16:54:15 +0530

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+document.getElementById("play").onclick = function () {
+    location.href = "screen2.html";
+};
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+ 
